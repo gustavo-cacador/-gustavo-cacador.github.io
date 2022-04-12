e88789436da3fb3f57f031f264f02cd7d0e964fd
gustavo-cacador <102495284+gustavo-cacador@users.noreply.github.com>
2022-04-12 14:22:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirecionarGithub(){
+    window.location.assign('https://github.com/gustavo-cacador')
+}
+function redirecionarLinkedin(){
+    window.location.assign("https://www.linkedin.com/in/gustavo-ca%C3%A7ador-a61473234/")
+}
